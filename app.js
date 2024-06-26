5f34b9949b3a710da57e52feff09c450021fbf64
zainxb119 <zainabaali002@email.com>
2024-06-26 16:36:01 +0300

    correction

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ Complete Exercise 11 in the space below:
 const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
 
 const odds = [];
-for (number in nums) {
+for (number of nums) {
     if (number % 2 === 1) 
         odds.push(number);
 }
